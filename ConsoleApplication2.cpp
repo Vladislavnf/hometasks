b21a818082668f9d522c286c41c3eef01768da9c
Vlad-Ridosh <vladislavridosh@gmail.com>
2025-09-03 22:54:02 +0300

    Перше завдання: слова довжиною 7 і більше літер

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+    ifstream input("input.txt");   
+    ofstream output("output2.txt"); 
+
+    if (!input.is_open() || !output.is_open()) {
+        cerr << "Помилка відкриття файлу!" << endl;
+        return 1;
+    }
+
+    string word;
+    while (input >> word) { 
+        if (word.length() >= 7) {
+            output << word << " "; 
+        }
+    }
+
+    input.close();
+    output.close();
+    cout << "Готово! Результат у output1.txt" << endl;
+    return 0;
+}
